d18a5731cc600df0431fb6fb19a2bfed4241fe89
Diego Umbelino <diego9us@gmail.com>
2024-10-18 21:19:51 -0300

    tool

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js')
+      .then(() => console.log('Service Worker registrado com sucesso!'))
+      .catch(err => console.error('Erro ao registrar Service Worker:', err));
+  }
+
 const cards = [
     { name: 'mico', img: 'https://i.pinimg.com/736x/36/e0/a8/36e0a8e582e30170842cd451eb3eb5b7.jpg' },
     { name: 'mico', img: 'https://i.pinimg.com/736x/36/e0/a8/36e0a8e582e30170842cd451eb3eb5b7.jpg' },
